e9418dda03357f086b0c33d41a027f898ce53e22
Andrew Nater <a.a.nater@gmail.com>
2017-01-30 14:51:36 -0500

    Add BorderSpaced component

--- src/components/lists/ListsExample.jsx
+++ src/components/lists/ListsExample.jsx
@@ -3,14 +3,16 @@ import React from "react";
 
 // Layout
 import BlockItemDottedBorder from './BlockItemDottedBorder';
+import BorderSpaced from './BorderSpaced';
 
 export default () => (
     <article id="lists" className="measure-wide center pt4">
         <h2 className="f2 lh-title">Lists</h2>
         <section id="aspect-ratio">
             <h3 className="f3 lh-copy">BlockItemDottedBorder</h3>
-            <BlockItemDottedBorder
-            />
+            <BlockItemDottedBorder/>
+            <h3 className="f3 lh-copy">BorderSpaced</h3>
+            <BorderSpaced/>
         </section>
     </article>
 );
