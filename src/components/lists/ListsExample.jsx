ae27713aa37afeca25006624f24a7aae3f8d9a42
Andrew Nater <a.a.nater@gmail.com>
2017-01-30 14:53:38 -0500

    Add BorderTight component

--- src/components/lists/ListsExample.jsx
+++ src/components/lists/ListsExample.jsx
@@ -4,6 +4,7 @@ import React from "react";
 // Layout
 import BlockItemDottedBorder from './BlockItemDottedBorder';
 import BorderSpaced from './BorderSpaced';
+import BorderTight from './BorderTight';
 
 export default () => (
     <article id="lists" className="measure-wide center pt4">
@@ -13,6 +14,8 @@ export default () => (
             <BlockItemDottedBorder/>
             <h3 className="f3 lh-copy">BorderSpaced</h3>
             <BorderSpaced/>
+            <h3 className="f3 lh-copy">BorderTight</h3>
+            <BorderTight/>
         </section>
     </article>
 );
