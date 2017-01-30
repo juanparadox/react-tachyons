c111a7f276cb9aeeab1072778ae541aad2ed617d
Andrew Nater <a.a.nater@gmail.com>
2017-01-30 15:11:45 -0500

    Add ContactPhone component

--- src/components/lists/ListsExample.jsx
+++ src/components/lists/ListsExample.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import BlockItemDottedBorder from './BlockItemDottedBorder';
 import BorderSpaced from './BorderSpaced';
 import BorderTight from './BorderTight';
+import ContactPhone from './ContactPhone';
 
 export default () => (
     <article id="lists" className="measure-wide center pt4">
@@ -12,10 +13,18 @@ export default () => (
         <section id="aspect-ratio">
             <h3 className="f3 lh-copy">BlockItemDottedBorder</h3>
             <BlockItemDottedBorder/>
+        </section>
+        <section id="border-spaced">
             <h3 className="f3 lh-copy">BorderSpaced</h3>
             <BorderSpaced/>
+        </section>
+        <section id="border-tight">
             <h3 className="f3 lh-copy">BorderTight</h3>
             <BorderTight/>
+        </section>
+        <section id="contact-phone">
+            <h3 className="f3 lh-copy">ContactPhone</h3>
+            <ContactPhone />
         </section>
     </article>
 );
