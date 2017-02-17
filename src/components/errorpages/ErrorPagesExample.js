5e0f18a505f8a884b7eac1865b5ac9c084fdaa67
Bernal <jbernalxx92@aol.com>
2017-02-17 15:55:11 -0500

    Finished Error Pages Example

---
+++ src/components/errorpages/ErrorPagesExample.js
@@ -0,0 +1,14 @@
+// React
+import React from "react";
+
+import FourOhFour from "./404.js";
+
+export default () => (
+	<article id="404" className="measure-wide center pt4">
+        <h2 className="f2 lh-title">Error Pages</h2>
+        <section id="404">
+            <h3 className="f3 lh-copy">404</h3>
+            <FourOhFour/>
+        </section>
+    </article>
+)
