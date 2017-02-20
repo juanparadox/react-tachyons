f45205d729b423132e01e91ac7802b4d9c65be0a
Andrew Nater <a.a.nater@gmail.com>
2017-02-20 16:18:54 -0500

    Add Simple component

--- src/components/definitionlists/DefinitionListsExample.js
+++ src/components/definitionlists/DefinitionListsExample.js
@@ -3,14 +3,18 @@ import React from "react";
 
 // Article Lists
 import Inline from './Inline';
-// import TitlePreviewAuthorMedia from './TitlePreviewAuthorMedia';
+import Simple from './Simple';
 
 export default () => (
     <article id="definition-lists" className="measure-wide center pt4">
         <h2 className="f2 lh-title">Definition Lists</h2>
-        <section id="title-preview-author-media-flipped">
+        <section>
             <h3 className="f3 lh-copy">Inline</h3>
             <Inline/>
+        </section>
+        <section>
+            <h3 className="f3 lh-copy">Simple</h3>
+            <Simple/>
         </section>
     </article>
 );
