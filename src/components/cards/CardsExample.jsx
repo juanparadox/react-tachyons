0a786d10fa51cdc426ce2a0d5154c6b323541310
Andrew Nater <a.a.nater@gmail.com>
2017-08-06 13:50:32 -0400

    Refactor AlbumCentered & AlbumLeft into a single Album component. Add BasicTextCard and NewsCard components

--- src/components/cards/CardsExample.jsx
+++ src/components/cards/CardsExample.jsx
@@ -2,13 +2,34 @@
 import React from "react";
 
 // Cards
-import AlbumCentered from "./AlbumCentered";
+import Album from "./Album";
+import BasicTextCard from "./BasicTextCard";
+import NewsCard from "./NewsCard";
 
 export default () =>
 	<article id="cards" className="center pt4">
 		<h2 className="f2 lh-title">Cards</h2>
-		<section id="aspect-ratio">
-			<h3 className="f3 lh-copy">AlbumCentered</h3>
-			<AlbumCentered />
+		<section id="album">
+			<h3 className="f3 lh-copy">Album</h3>
+			<code className="db mb3">Props: artistName, albumName, url, img, align</code>
+
+			<Album />
+			<code className="db mb3">align="left" (default)</code>
+
+			<Album align="center" />
+			<code className="db mb3">align="center"</code>
+
+			<Album align="right" />
+			<code className="db mb3">align="right"</code>
+		</section>
+		<section id="basic-text-card">
+			<h3 className="f3 lh-copy">BasicTextCard</h3>
+			<code className="db mb3">Props: title, text, bgColor</code>
+			<BasicTextCard />
+		</section>
+		<section id="news-card">
+			<h3 className="f3 lh-copy">NewsCard</h3>
+			<code className="db mb3">Props: publisher, img, headline, url, source, timestamp</code>
+			<NewsCard />
 		</section>
 	</article>;
