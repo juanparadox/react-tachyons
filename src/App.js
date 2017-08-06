cad37daca1284952b2bd552bbc504f3b295f42a3
Andrew Nater <a.a.nater@gmail.com>
2017-08-06 14:20:14 -0400

    Add CardsExample

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // React
-import React, {Component} from "react";
+import React, { Component } from "react";
 // eslint-disable-next-line
 import tachyons from "tachyons"; // TODO: determine the best way to load tachyons in...
 
@@ -24,7 +24,7 @@ import ListsExample from "./components/lists/ListsExample";
 // Nav
 import NavExample from "./components/nav/NavExample";
 
- // Error Pages
+// Error Pages
 import ErrorPagesExample from "./components/errorpages/ErrorPagesExample";
 
 // Footer
@@ -54,38 +54,42 @@ import QuotesExample from "./components/quotes/QuotesExample";
 // tables
 import TablesExample from "./components/tables/TablesExample";
 
+// Cards
+import CardsExample from "./components/cards/CardsExample";
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Basic
-          tagline="React.JS + Tachyons component library"
-          subline="stateless react components and a whole lot of tachyons"
-          small="Want to contribute?"
-          leftButton={{ text: "GitHub", href: "https://github.com/juanparadox/react-tachyons", bgColor: "bg-dark-green"}}
-          rightButton={{ text: "Issues", href: "https://github.com/juanparadox/react-tachyons/issues"}}
-        />
-        <section className="ph5">
-          <TextExample/>
-          <LinksExample/>
-          <ButtonExample/>
-          <FormExample/>
-          <LayoutExample/>
-          <ListsExample/>
-          <ErrorPagesExample/>
-          <FooterExample/>
-          <HeadersExample/>
-          <ArticleListsExample/>
-          <ArticlesExample/>
-          <BannersExample/>
-          <DefinitionListsExample />
-          <QuotesExample />
-          <TablesExample />
-          <NavExample/>
-        </section>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Basic
+					tagline="React.JS + Tachyons component library"
+					subline="stateless react components and a whole lot of tachyons"
+					small="Want to contribute?"
+					leftButton={{ text: "GitHub", href: "https://github.com/juanparadox/react-tachyons", bgColor: "bg-dark-green" }}
+					rightButton={{ text: "Issues", href: "https://github.com/juanparadox/react-tachyons/issues" }}
+				/>
+				<section className="ph5">
+					<TextExample />
+					<LinksExample />
+					<ButtonExample />
+					<FormExample />
+					<LayoutExample />
+					<ListsExample />
+					<ErrorPagesExample />
+					<FooterExample />
+					<HeadersExample />
+					<ArticleListsExample />
+					<ArticlesExample />
+					<BannersExample />
+					<DefinitionListsExample />
+					<QuotesExample />
+					<TablesExample />
+					<NavExample />
+					<CardsExample />
+				</section>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
